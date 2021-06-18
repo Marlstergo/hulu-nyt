19712d20dd11d3aca0ef88af288bf0a99cd60554
Marlstergo <abdulmaliqshola@gmail.com>
2021-06-18 12:30:30 +0100

    hulu

--- components/result.js
+++ components/result.js
@@ -1,11 +1,16 @@
 import React from "react";
-
+import Thumnail from "./thumnail";
+import FlipMove from 'react-flip-move'
 // import request from '../utility/request';
 
-function Result() {
-  return <div>
-
-  </div>;
+function Result({results}) {
+  return <FlipMove className=' px-5 my-10 sm:grid md:grid-cols-2 lg:grid-cols-3 3xl:flex flex-wrap justify-center'>
+    {
+      results.map((result) =>(
+        <Thumnail key={result.id} result={result}/>
+      ))
+    }
+  </FlipMove>;
 }
 
 export default Result;
--- utility/request.js
+++ utility/request.js
@@ -1,13 +1,14 @@
 const API_KEY = process.env.API_KEY;
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   fetchTrending:{
     title: 'Trending',
     url: `/trending/all/week?api_key=${API_KEY}&language=en-US`
   },
   fetchTopRated:{
-    title: 'Trending',
-    url: `/movie/top_rated?week?api_key=${API_KEY}&language=en-US`
+    title: 'Top Rated',
+    url: `/movie/top_rated?api_key=${API_KEY}&language=en-US`
   },
   fetchActionMovies:{
     title: 'Action',
